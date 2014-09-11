a974a63a272878b16c1d9ca4a769bd7e9b1acd01
Alex Mayzlah <mayzlah@yandex.ru>
2014-09-11 22:26:36 +0400

    Networking, step 1
    Servers are creating and server list is receiving

--- R&D games/Assets/Scripts/FireProjectile.cs
+++ R&D games/Assets/Scripts/FireProjectile.cs
@@ -35,3 +35,6 @@ public class FireProjectile : MonoBehaviour {
 	}
 
 }
+
+
+
--- R&D games/Assets/Scripts/MainMenu.cs
+++ R&D games/Assets/Scripts/MainMenu.cs
@@ -3,27 +3,190 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
-	public float topShift = Screen.height*0.25f;
-	public float leftShift = Screen.width*0.25f;
-	public float buttonWidth = Screen.width*0.5f;
-	public float buttonHeight = Screen.height*0.1f;
-	public float buttonGap = Screen.height*0.125f;
-
-	void OnGUI () {
+	private float topShift;
+	private float leftShift;
+	private float buttonWidth;
+	private float buttonHeight;
+	private float buttonGap;
 	
-		if (GUI.Button (new Rect (Screen.width * 0.25f, Screen.height * 0.25f, Screen.width * 0.5f, Screen.height * 0.1f), "Start")) {
-						Application.LoadLevel ("proto");
-				}
-		if (GUI.Button (new Rect (Screen.width*0.25f, Screen.height*0.375f, Screen.width*0.5f, Screen.height*0.1f), "Host")) {
-			
+	public string ipAddressMS = "88.198.178.232";
+	public int portMS = 48151;
+	public int port = 48151;
+	public string gameTypeName = "maizlach_proto";
+	public string serverName = "Maizlach server";
+
+	private string serverResult;
+
+	private bool isMainMenu = true;
+	private bool isServerStart = false;
+	private bool isServer = false;
+	private bool isClientStart = false;
+	private bool isClient = false;
+
+	private HostData[] serverList;
+	public int numberOfServers;
+
+	public void StartServer(){
+		Debug.Log(Network.InitializeServer(4, port, !Network.HavePublicAddress()));
+
+	}
+
+	void OnServerInitialized(){
+		MasterServer.ipAddress = ipAddressMS;
+		MasterServer.port = portMS;
+		MasterServer.RegisterHost(gameTypeName,serverName);
+	}
+
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if(msEvent == MasterServerEvent.RegistrationSucceeded)
+		{
+			serverResult = "Uspekhi!";
 		}
-		if (GUI.Button (new Rect (Screen.width*0.25f, Screen.height*0.5f, Screen.width*0.5f, Screen.height*0.1f), "Connect")) {
-			
+		else if(msEvent == MasterServerEvent.HostListReceived)
+		{
+			serverList = MasterServer.PollHostList();
+			numberOfServers = serverList.Length;
+			Debug.Log (msEvent);
 		}
-		if (GUI.Button (new Rect (Screen.width*0.25f, Screen.height*0.625f, Screen.width*0.5f, Screen.height*0.1f), "Exit")) {
+		else {
+			Debug.Log (msEvent);
+		}
+	}
+	private void GetServerList(){
+		MasterServer.ipAddress = ipAddressMS;
+		MasterServer.port = portMS;
+		MasterServer.RequestHostList(gameTypeName);
+	}
+
+
+	
+	private void MainMenuGUI () {
+		if (GUI.Button (new Rect (Screen.width * 0.25f
+		                          ,Screen.height * 0.25f
+		                          ,Screen.width * 0.5f
+		                          ,Screen.height * 0.1f), "Start")) {
+			Application.LoadLevel ("proto");
+		}
+		if (GUI.Button (new Rect (Screen.width*0.25f
+		                          ,Screen.height*0.375f
+		                          ,Screen.width*0.5f
+		                          ,Screen.height*0.1f), "Host")) {
+			isMainMenu = false;
+			isServerStart = true;
+		}
+		if (GUI.Button (new Rect (Screen.width*0.25f
+		                          ,Screen.height*0.5f
+		                          ,Screen.width*0.5f
+		                          ,Screen.height*0.1f), "Connect")) {
+			isMainMenu = false;
+			isClientStart = true;
+		}
+		if (GUI.Button (new Rect (Screen.width*0.25f
+		                          ,Screen.height*0.625f
+		                          ,Screen.width*0.5f
+		                          ,Screen.height*0.1f), "Exit")) {
 			Application.Quit();
 		}
-	
+	}
+
+	private void ServerStartGUI(){
+		GUI.Label(new Rect(Screen.width*.2f
+		                   ,Screen.height*.2f
+		                   ,Screen.width*.2f
+		                   ,Screen.height*.2f), "Server Name");
+		serverName = GUI.TextField(new Rect(Screen.width*.5f
+		                                    ,Screen.height*.2f
+		                       				,Screen.width*.2f
+		                       				,Screen.height*.2f), serverName);
+		if(GUI.Button(new Rect(Screen.width*.2f
+		                       ,Screen.height*.5f
+		                       ,Screen.width*.2f
+		                       ,Screen.height*.2f), "Back")){
+			isServerStart = false;
+			isMainMenu = true;
+		}
+		if(GUI.Button(new Rect(Screen.width*.5f
+		                       ,Screen.height*.5f
+		                       ,Screen.width*.2f
+		                       ,Screen.height*.2f), "Start")){
+			isServerStart = false;
+			isServer = true;
+			serverResult = "Waiting...";
+			StartServer();
+		}
+	}
+
+	private void ServerGUI(){
+		GUI.Label(new Rect(Screen.width*.2f
+		                   ,Screen.height*.2f
+		                   ,Screen.width*.2f
+		                   ,Screen.height*.2f), serverResult);
+		if(GUI.Button(new Rect(Screen.width*.2f
+		                       ,Screen.height*.5f
+		                       ,Screen.width*.2f
+		                       ,Screen.height*.2f), "Back")){
+			isServer = false;
+			isServerStart = true;
+			MasterServer.UnregisterHost();
+			Network.Disconnect();
+		}
+	}
+
+	private void ClientStartGUI(){
+		GUI.Label(new Rect(Screen.width*.2f
+		                   ,Screen.height*.2f
+		                   ,Screen.width*.2f
+		                   ,Screen.height*.2f), "Server List");
+		if(GUI.Button(new Rect(Screen.width*.2f
+		                       ,Screen.height*.5f
+		                       ,Screen.width*.2f
+		                       ,Screen.height*.2f), "Back")){
+			isClientStart = false;
+			isMainMenu = true;
+		}
+		if(numberOfServers == 0)
+		{
+			GUI.Label(new Rect(Screen.width*.6f
+			                   ,Screen.height*.2f
+			                   ,Screen.width*.2f
+			                   ,Screen.height*.2f), "No Servers Found");
+		}
+		else{
+			GUI.Label(new Rect(Screen.width*.6f
+			                   ,Screen.height*.2f
+			                   ,Screen.width*.2f
+			                   ,Screen.height*.2f), serverList[0].gameName);
+		}
+
+	}
+
+
+	void Start() {
+		topShift = Screen.height*0.25f;
+		leftShift = Screen.width*0.25f;
+		buttonWidth = Screen.width*0.5f;
+		buttonHeight = Screen.height*0.1f;
+		buttonGap = Screen.height*0.125f;
+
+	}
+	void OnGUI () {
+		if(isMainMenu)
+		{
+			MainMenuGUI();
+		}
+		if(isServerStart){
+			ServerStartGUI();
+		}
+		if(isServer)
+		{
+			ServerGUI();
+		}
+		if(isClientStart)
+		{
+			GetServerList();
+			ClientStartGUI();
+		}
 	}
 }
 
